5dca94c97d9b76322f23a9bdaba3cebe3788c611
BlondieSC <2057252500@qq.com>
2024-12-27 11:54:19 +0800

    系统查询模块

---
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/CourseCategoryController.java
@@ -0,0 +1,13 @@
+package com.xuecheng.system.controller;
+
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseCategoryController {
+    @GetMapping("/course-category/tree-nodes")
+    public CourseCategoryTreeDto queryTreeNodes() {
+        return null;
+    }
+}
